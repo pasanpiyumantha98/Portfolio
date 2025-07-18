076ffd8b96534621a384632a7af92e78052f4d7d
pasanpiyumantha98 <139437240+pasanpiyumantha98@users.noreply.github.com>
2025-07-18 19:28:48 +0530

    p4

---
+++ front/src/assets/components/NavBar.jsx
@@ -0,0 +1,60 @@
+import "../css/bootstrap.min.css"
+import "../css/bootstrap-icons.css"
+import "../css/magnific-popup.css"
+import "../css/templatemo-first-portfolio-style.css"
+import logo from "../images/logo4.png"
+
+function NavBar ()
+{
+return (
+
+ <nav class="navbar navbar-expand-lg">
+            <div class="container">
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <a href="index.html" class="navbar-brand mx-auto mx-lg-0"><img src={logo}></img></a>
+
+                <div class="d-flex align-items-center d-lg-none">
+                    <i class="navbar-icon bi-telephone-plus me-3"></i>
+                    <a class="custom-btn btn" href="#section_5">
+                        120-240-9600
+                    </a>
+                </div>
+
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-lg-5">
+                        <li class="nav-item">
+                            <a class="nav-link click-scroll" href="#section_1">Home</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link click-scroll" href="#section_2">About Me</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link click-scroll" href="#section_3">Projects</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link click-scroll" href="#section_4">Education</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link click-scroll" href="#section_5">Contact</a>
+                        </li>
+                    </ul>
+
+                    
+                </div>
+
+            </div>
+        </nav>
+
+)
+
+}
+
+export default NavBar
--- front/src/assets/pages/home.jsx
+++ front/src/assets/pages/home.jsx
@@ -5,6 +5,7 @@ import "../css/templatemo-first-portfolio-style.css"
 import image1 from "../images/1.jpg"
 import image2 from "../images/3.png"
 import logo from "../images/logo4.png"
+import NavBar from "../components/NavBar"
 
 
 function Home(){
@@ -13,50 +14,7 @@ return(
 
        
 
-        <nav class="navbar navbar-expand-lg">
-            <div class="container">
-
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <a href="index.html" class="navbar-brand mx-auto mx-lg-0"><img src={logo}></img></a>
-
-                <div class="d-flex align-items-center d-lg-none">
-                    <i class="navbar-icon bi-telephone-plus me-3"></i>
-                    <a class="custom-btn btn" href="#section_5">
-                        120-240-9600
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ms-lg-5">
-                        <li class="nav-item">
-                            <a class="nav-link click-scroll" href="#section_1">Home</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link click-scroll" href="#section_2">About Me</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link click-scroll" href="#section_3">Projects</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link click-scroll" href="#section_4">Education</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link click-scroll" href="#section_5">Contact</a>
-                        </li>
-                    </ul>
-
-                    
-                </div>
-
-            </div>
-        </nav>
+       <NavBar/>
 
         <main>
 
